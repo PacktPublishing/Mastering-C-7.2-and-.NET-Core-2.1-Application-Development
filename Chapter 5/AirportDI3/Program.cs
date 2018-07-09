e9745beb3e89e59079ca163c58e782ba5f3bbec3
Conrad Akunga <cakunga@innova.co.ke>
2018-07-10 00:44:30 +0300

    + Method injection

---
+++ Chapter 5/AirportDI3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AirportDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a jet plane
+            var jetEngine = new JetEngine();
+            var fokker = new Plane();
+            fokker.Start(jetEngine);
+        }
+    }
+}
