2acf73209e02a6ab9b899ef3d213c78ab6cc1a28
Conrad Akunga <conradakunga@gmail.com>
2018-08-20 16:20:52 +0300

    + Measure using datetime comparisons

---
+++ Chapter 7/Manual/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Manual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            GenerateWithString();
+            var stop = DateTime.Now;
+            Console.WriteLine((stop - start).TotalSeconds);
+        }
+        static string GenerateWithString()
+        {
+            var str = "";
+            for (var i = 0; i < 100_000; i++)
+            {
+                str += i.ToString();
+            }
+            return str;
+        }
+    }
+}
