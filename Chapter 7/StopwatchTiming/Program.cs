bcd614edfffef7a36bf6bf36157789d942e6b918
Conrad Akunga <conradakunga@gmail.com>
2018-08-20 18:20:35 +0300

    + Stopwatch

---
+++ Chapter 7/StopwatchTiming/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopwatchTiming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sp = new Stopwatch();
+            sp.Start();
+            GenerateWithString();
+            sp.Stop();
+            Console.WriteLine(sp.Elapsed.TotalMilliseconds);
+        }
+        static string GenerateWithString()
+        {
+            var str = "";
+            for (var i = 0; i < 100_000; i++)
+            {
+                str += i.ToString();
+            }
+            return str;
+        }
+    }
+}
