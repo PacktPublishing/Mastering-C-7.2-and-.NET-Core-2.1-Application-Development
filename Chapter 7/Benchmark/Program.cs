b3be50372232fdc0be21857cc0bb4afc3c76ad6c
Conrad Akunga <conradakunga@gmail.com>
2018-08-20 19:22:29 +0300

    + Default benchmark

---
+++ Chapter 7/Benchmark/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    public class Program
+    {
+        public class GeneratorTests
+        {
+            public GeneratorTests()
+            {
+                GenerateWithString();
+                GenerateWithStringBuilder();
+            }
+            [Benchmark]
+            public string GenerateWithString()
+            {
+                var str = "";
+                for (var i = 0; i < 1_000; i++)
+                {
+                    str += i.ToString();
+                }
+                return str;
+            }
+            [Benchmark]
+            public string GenerateWithStringBuilder()
+            {
+                var sb = new StringBuilder();
+                for (var i = 0; i < 1_000; i++)
+                {
+                    sb.Append(i.ToString());
+                }
+                return sb.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<GeneratorTests>();
+        }
+
+    }
+}
