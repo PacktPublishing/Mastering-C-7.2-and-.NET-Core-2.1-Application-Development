8be68edca5fcaedfcfce2b0ccca0ab72d46e5024
Conrad Akunga <conradakunga@gmail.com>
2018-08-21 15:57:57 +0300

    + VS Profiler with StringBuilder

---
+++ Chapter 7/VSProfile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace VSProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateWithStringBuilder();
+            Console.ReadLine();
+        }
+        static string GenerateWithStringBuilder()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < 5_000; i++)
+            {
+                sb.Append(i.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
