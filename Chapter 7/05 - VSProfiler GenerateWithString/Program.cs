bed39964175137cf430061c921193d30e4e354c9
Conrad Akunga <conradakunga@gmail.com>
2018-08-21 16:23:47 +0300

    # Refactor to allow profiler to collect data

--- Chapter 7/05 - VSProfiler GenerateWithString/Program.cs
+++ Chapter 7/05 - VSProfiler GenerateWithString/Program.cs
@@ -1,15 +1,19 @@
-﻿namespace VSProfiler
+﻿using System;
+using System.Threading;
+
+namespace VSProfiler
 {
     class Program
     {
         static void Main(string[] args)
         {
             GenerateWithString();
+            Thread.Sleep(TimeSpan.FromSeconds(5));
         }
         static string GenerateWithString()
         {
             var str = "";
-            for (var i = 0; i < 5000; i++)
+            for (var i = 0; i < 1_000; i++)
             {
                 str += i.ToString();
             }
