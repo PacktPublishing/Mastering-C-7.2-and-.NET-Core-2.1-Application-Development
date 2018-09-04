ff51e984ee95613a721257b3114dd7907b4e8424
Conrad Akunga <conradakunga@gmail.com>
2018-09-04 16:47:39 +0300

    + Init

---
+++ Chapter 8/EFProfiling/Models/CustomerCustomerDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFProfiling.Models
+{
+    public partial class CustomerCustomerDemo
+    {
+        public string CustomerId { get; set; }
+        public string CustomerTypeId { get; set; }
+
+        public Customers Customer { get; set; }
+        public CustomerDemographics CustomerType { get; set; }
+    }
+}
---
+++ Chapter 8/EFProfiling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using EFProfiling.Models;
+using HibernatingRhinos.Profiler.Appender.EntityFramework;
+
+namespace EFProfiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntityFrameworkProfiler.Initialize();
+            using (var ctx = new NorthwndContext())
+            {
+                var query = ctx.Customers.Where(x => x.CompanyName.StartsWith("A")).ToList();
+                foreach (var result in query)
+                    Console.WriteLine($"Company: {result.CompanyName}");
+            }
+        }
+    }
+}
+
