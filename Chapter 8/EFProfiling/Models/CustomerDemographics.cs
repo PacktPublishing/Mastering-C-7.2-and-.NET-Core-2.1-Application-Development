72fff8b51ee65705ef7334ad87254d08a1703fcd
Conrad Akunga <conradakunga@gmail.com>
2018-09-04 15:58:10 +0300

    + Reverse enginner database

---
+++ Chapter 8/EFProfiling/Models/CustomerDemographics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFProfiling.Models
+{
+    public partial class CustomerDemographics
+    {
+        public CustomerDemographics()
+        {
+            CustomerCustomerDemo = new HashSet<CustomerCustomerDemo>();
+        }
+
+        public string CustomerTypeId { get; set; }
+        public string CustomerDesc { get; set; }
+
+        public ICollection<CustomerCustomerDemo> CustomerCustomerDemo { get; set; }
+    }
+}
