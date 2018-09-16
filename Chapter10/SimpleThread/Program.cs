732bb8ffb6c2d73c4ec294e9b1462093b14661c9
Conrad Akunga <conradakunga@gmail.com>
2018-09-16 22:20:23 +0300

    Thread interruption

---
+++ Chapter10/SimpleThread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Thread(ChildThread);
+            t.Name = "Interrupt";
+            t.Start();
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            t.Interrupt();
+        }
+        static void ChildThread()
+        {
+            try
+            {
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (ThreadInterruptedException)
+            {
+                Console.WriteLine($"Thread '{Thread.CurrentThread.Name}' was interrupted");
+            }
+            finally
+            {
+                //Any cleanup here
+            }
+        }
+    }
+}
