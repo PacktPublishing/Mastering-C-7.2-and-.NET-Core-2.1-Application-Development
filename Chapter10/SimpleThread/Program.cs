5284e0228c62b95d1c47590fcd3bf33d25e50a96
Conrad Akunga <conradakunga@gmail.com>
2018-09-16 23:42:03 +0300

    Background threads

---
+++ Chapter10/SimpleThread/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new Thread(() => { Console.WriteLine("Starting a thread"); Thread.SpinWait(1000); });
+            Console.WriteLine($"Thread is background? {thread.IsBackground}");
+        }
+    }
+}
