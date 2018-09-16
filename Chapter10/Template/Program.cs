3cd14b33fa0779601fb1c4d31f29bc3223368447
Conrad Akunga <conradakunga@gmail.com>
2018-09-16 21:50:12 +0300

    # Thread abort

---
+++ Chapter10/Template/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+namespace SimpleThread
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var t = new Thread(Greet);
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            t.Abort();
+            Console.WriteLine("Completed");
+        }
+        static void Greet()
+        {
+            Thread.SpinWait((int)TimeSpan.FromMinutes(1).TotalMilliseconds);
+            Console.WriteLine("This will never print");
+        }
+    }
+}
