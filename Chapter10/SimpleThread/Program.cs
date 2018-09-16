64028a728fef72d56e67e09386cf5ae8f902a437
Conrad Akunga <conradakunga@gmail.com>
2018-09-16 22:56:11 +0300

    Thread joining

---
+++ Chapter10/SimpleThread/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new Thread(() => Console.WriteLine("Hello from the first child"));
+            first.Start();
+            var second = new Thread(() => Console.WriteLine("Hello from the second child"));
+            second.Start();
+            first.Join();
+            second.Join();
+            Console.WriteLine("The parent waited for the children");
+        }
+    }
+}
