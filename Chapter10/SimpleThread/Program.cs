4826e8b328ddffa3e007e070e8b66395348251d6
Conrad Akunga <conradakunga@gmail.com>
2018-09-16 16:49:35 +0300

    Parameterized thread start

---
+++ Chapter10/SimpleThread/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tMary = new Thread(Program.Greet);
+            tMary.Start("Mary");
+        }
+        static void Greet(object Name)
+        {
+            Console.WriteLine($"Hello {Name} from {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
