7ff3a273cc461be2af12cb792ae2fa6b92b16487
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 09:40:49 +0300

    Parallel Invoke

---
+++ Chapter10/11 - Parallel Invoke/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Parallel.Invoke(
+                () => Console.WriteLine(client.GetStringAsync("https://www.amazon.com").Result),
+                () => Console.WriteLine(client.GetStringAsync("https://www.google.com").Result),
+                () => Console.WriteLine(client.GetStringAsync("https://www.microsoft.com").Result));
+        }
+    }
+}
