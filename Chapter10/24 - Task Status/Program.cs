e8805c5a2d883502456c8aafcad1e814c68aaced
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 18:49:06 +0300

    Task status

---
+++ Chapter10/24 - Task Status/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task(() => Console.WriteLine("This is a task"));
+            Console.WriteLine($"Task status is {task.Status}");
+            task.Start();
+            Console.WriteLine($"Task status is {task.Status}");
+            task.Wait();
+            Console.WriteLine($"Task status is {task.Status}");
+        }
+    }
+}
