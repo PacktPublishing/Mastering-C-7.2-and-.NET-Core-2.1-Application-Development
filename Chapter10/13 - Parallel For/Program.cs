a970a9947749f7e1b623180650db93d9b9fc82cd
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 10:27:40 +0300

    Parallel For

---
+++ Chapter10/13 - Parallel For/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 65; i < 91; i++)
+                Print((char)i);
+            Console.WriteLine();
+            Parallel.For(65, 91, x => Print((char)x));
+        }
+        static void Print(char s)
+        {
+            Console.Write(s);
+        }
+    }
+}
