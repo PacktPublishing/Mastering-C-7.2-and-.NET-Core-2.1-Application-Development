0f4341c556d50deda9697fc0fa59e3410949c41e
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 09:27:27 +0300

    Thread priority

---
+++ Chapter10/SimpleThread/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var low = new Thread(Lower);
+            var high = new Thread(Higher);
+            high.Priority= ThreadPriority.Highest;
+            low.Start();
+            high.Start();
+        }
+        static void Lower() => Console.WriteLine($"Hello. My priority is {Thread.CurrentThread.Priority}");
+        static void Higher() => Console.WriteLine($"Hello. My priority is {Thread.CurrentThread.Priority}");
+    }
+}
