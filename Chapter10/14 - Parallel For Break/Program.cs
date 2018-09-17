315c46860ecf380870dde4e0ffde5dddfc5e200a
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 14:03:10 +0300

    Parallel.For with a break

---
+++ Chapter10/14 - Parallel For Break/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Parallel.For(65, 91, (x, state) =>
+            {
+                // Exit when we get to 75
+                if (x == 75)
+                    state.Break();
+                Print((char)x);
+            });
+            Console.WriteLine();
+            Console.WriteLine($"Did we complete? {result.IsCompleted}. We broke at {result.LowestBreakIteration}");
+        }
+        static void Print(char c) => Console.Write(c);
+    }
+
+}
