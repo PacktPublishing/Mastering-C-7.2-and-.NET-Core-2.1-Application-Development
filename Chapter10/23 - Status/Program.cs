04014248b235d30dd2b90909c26662e90f166eb4
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 18:44:46 +0300

    Thread status

---
+++ Chapter10/23 - Status/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new Thread(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            });
+            Console.WriteLine($"State is {thread.ThreadState}");
+            thread.Start();
+            Console.WriteLine($"State is {thread.ThreadState}");
+            thread.Join();
+            Console.WriteLine($"State is {thread.ThreadState}");
+        }
+    }
+}
