db26156a06c3f3478cf652a500812f3a18c4db2a
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 15:01:23 +0300

    Task factory

---
+++ Chapter10/17 - TaskFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Starting ...");
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                Console.WriteLine("Done");
+            });
+            task.Wait();
+            Console.WriteLine("Completed");
+        }
+    }
+}
