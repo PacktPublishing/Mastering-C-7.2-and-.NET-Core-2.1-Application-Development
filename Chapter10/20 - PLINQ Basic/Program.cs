7632f5e958b914acb6239794e35a06887fa8ea69
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 18:04:32 +0300

    PLINQ

---
+++ Chapter10/20 - PLINQ Basic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace SimpleThread
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BenchmarkRunner.Run<Program>();
+        [Benchmark]
+        public double[] Sequential()
+        {
+            var numbers = Enumerable.Range(0, 10_000);
+            return numbers.Select(x => Math.Sqrt(x)).ToArray();
+        }
+        [Benchmark]
+        public double[] Parallel()
+        {
+            var numbers = Enumerable.Range(0, 10_000);
+            return numbers.Select(x => Math.Sqrt(x)).AsParallel().ToArray();
+        }
+
+    }
+}
