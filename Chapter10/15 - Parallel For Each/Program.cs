9a6d4b817ff3b3d2fb1ca7dcac75041052f1d645
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 14:35:34 +0300

    Parallel for each

---
+++ Chapter10/15 - Parallel For Each/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = "The quick brown fox jumped over the lazy dog".Split(" ");
+            Parallel.ForEach(items, x =>
+            {
+                Console.WriteLine($"Hash of {x} is {BCrypt.Net.BCrypt.HashPassword(x, 16)}");
+            });
+        }
+
+    }
+}
