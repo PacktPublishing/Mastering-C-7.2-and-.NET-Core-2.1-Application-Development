c90ebab213f7d19ac1d9425fac6fd1ee66a77952
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 19:37:04 +0300

    Exception handling

---
+++ Chapter10/26 - Exception Handling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var items = "The quick brown fox jumped over the lazy dog".Split(" ");
+                Parallel.ForEach(items, x =>
+                {
+                    Console.WriteLine($"Hash of {x} is {BCrypt.Net.BCrypt.HashPassword(x, 16)}");
+                });
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var exception in ex.Flatten().InnerExceptions)
+                {
+                    // Handle the exceptions here
+                }
+            }
+        }
+    }
+}
