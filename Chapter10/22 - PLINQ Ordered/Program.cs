16ed3fdcbe361fe75a69f73ea933e4cdc4d39953
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 18:28:38 +0300

    PLINQ Ordered

---
+++ Chapter10/22 - PLINQ Ordered/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 30);
+            var result = numbers.AsParallel().AsOrdered().Where(x => x % 3 == 0).ToArray();
+            Print(result);
+        }
+        static void Print(int[] numbers)
+        {
+            foreach (var number in numbers)
+                Console.Write($"{number},");
+            Console.WriteLine();
+        }
+    }
+}
