6613a794160022c9a86d51df6f35f5b05be33a90
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 15:32:26 +0300

    Parent & child tasks

---
+++ Chapter10/19 - Task Parent & Children/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parent = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Hello from the parent");
+
+                var detached = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Hello from detached task");
+                });
+
+                var child = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Hello from child");
+                }, TaskCreationOptions.AttachedToParent);
+            });
+
+            parent.Wait();
+            Console.WriteLine("Completed");
+        }
+    }
+}
