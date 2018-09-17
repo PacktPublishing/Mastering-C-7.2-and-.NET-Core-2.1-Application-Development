c6462b64a38249f753fbc981005af0c828d0ec0b
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 14:48:57 +0300

    Task creation with task

---
+++ Chapter10/16 - Plain Task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task(Greet);
+            task.Start();
+            Console.WriteLine("Continuing with other work");
+            Console.Read();
+        }
+        static void Greet()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            Console.WriteLine("Greetings");
+        }
+    }
+}
