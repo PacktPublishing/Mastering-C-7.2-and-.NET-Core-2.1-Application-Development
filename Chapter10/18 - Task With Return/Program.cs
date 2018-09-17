8c983a5e0e9b868eec2e133c9ecd542c0af3419f
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 15:09:45 +0300

    Generic task

---
+++ Chapter10/18 - Task With Return/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Factory.StartNew<string>(GetIP);
+            Console.WriteLine(task.Result);
+        }
+        static string GetIP()
+        {
+            var client = new HttpClient();
+            return client.GetStringAsync("https://api.ipify.org").Result;
+        }
+    }
+}
