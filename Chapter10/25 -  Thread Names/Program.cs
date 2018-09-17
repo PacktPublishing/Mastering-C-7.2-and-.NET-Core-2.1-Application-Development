99285c346b7b39011dd77a9cd5e2d6ab63d12b67
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 18:56:39 +0300

    Thread names

---
+++ Chapter10/25 -  Thread Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new Thread(() => Console.WriteLine($"Hello from thread {Thread.CurrentThread.Name}"));
+            thread.Name = "MyThreadName";
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
