81e4c697e25bd2091a672e29adfe7cc4d75ef746
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 20:01:51 +0300

    Concurrent data structures

---
+++ Chapter10/27 - Shared State/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var addresses = new ConcurrentBag<String>();
+            var sites = new string[] { "www.google.com", "www.amazon.com", "www.apple.com",
+                "www.microsoft.com", "www.facebook.com" };
+            Parallel.ForEach(sites, x =>
+            {
+                addresses.Add(System.Net.Dns.GetHostEntry(x).AddressList.First().ToString());
+            });
+            foreach (var address in addresses)
+            {
+                Console.WriteLine(address);
+            }
+        }
+    }
+}
