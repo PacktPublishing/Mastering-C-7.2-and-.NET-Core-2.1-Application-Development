9f237ced436dba164f65ec0de240aed9ebb57198
Conrad Akunga <conradakunga@gmail.com>
2018-09-17 09:49:54 +0300

    Parallel Invoke overload

---
+++ Chapter10/12 - Parallel Invoke Overload/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace SimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var options = new ParallelOptions() { MaxDegreeOfParallelism = 3 };
+            Parallel.Invoke(options,
+                () => Console.WriteLine(client.GetStringAsync("https://www.amazon.com").Result),
+                () => Console.WriteLine(client.GetStringAsync("https://www.google.com").Result),
+                () => Console.WriteLine(client.GetStringAsync("https://www.apple.com").Result),
+                () => Console.WriteLine(client.GetStringAsync("https://www.microsoft.com").Result));
+        }
+    }
+}
