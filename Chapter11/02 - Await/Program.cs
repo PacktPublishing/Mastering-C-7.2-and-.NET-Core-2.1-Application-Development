d96f0f1e87a85957ed05b01b53001ed655f6e4ff
Conrad Akunga <conradakunga@gmail.com>
2018-09-24 00:34:22 +0300

    await

---
+++ Chapter11/02 - Await/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var homePage = await client.GetStringAsync("https://www.packtpub.com");
+            Console.WriteLine(homePage);
+        }
+    }
+}
