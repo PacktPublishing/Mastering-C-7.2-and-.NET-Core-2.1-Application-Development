92379bf7f080c21810c55f1930c86876c34a1076
Conrad Akunga <conradakunga@gmail.com>
2018-09-23 23:41:30 +0300

    + Asyc task

---
+++ Chapter 11/TaskSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TaskSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var resultTask = client.GetStringAsync("https://www.packtpub.com");
+            Console.WriteLine("Performing other operations...");
+            //Now obtain the resu;t
+            Console.WriteLine(resultTask.Result);
+        }
+    }
+}
