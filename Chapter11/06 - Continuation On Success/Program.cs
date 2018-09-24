88cbc1c8dcfc974f626dfbea23bf8da98de8922b
Conrad Akunga <conradakunga@gmail.com>
2018-09-24 09:13:33 +0300

    Continuation

---
+++ Chapter11/06 - Continuation On Success/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTask = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("I am running first");
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+            });
+            var secondTask = firstTask.ContinueWith(previous =>
+            {
+                Console.WriteLine("I am running second");
+            });
+            firstTask.Wait();
+        }
+    }
+}
