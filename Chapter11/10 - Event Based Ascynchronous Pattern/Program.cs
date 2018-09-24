f0a7676f069109f0ec4ab78ae6d3468d3ac62700
Conrad Akunga <conradakunga@gmail.com>
2018-09-24 14:56:38 +0300

    Event bassed ascynronous pattern

---
+++ Chapter11/10 - Event Based Ascynchronous Pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace TaskSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wc = new WebClient();
+            wc.DownloadStringAsync(new Uri("https://www.packtpub.com"));
+            // Wire up the event handler
+            wc.DownloadStringCompleted += (object sender, DownloadStringCompletedEventArgs e) =>
+            {
+                Console.WriteLine(e.Result);
+            };
+        }
+    }
+}
