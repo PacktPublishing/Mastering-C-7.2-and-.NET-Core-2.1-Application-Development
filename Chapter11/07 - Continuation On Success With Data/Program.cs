db8b4c4cfbe7faec96e92353395fd844611c416f
Conrad Akunga <conradakunga@gmail.com>
2018-09-24 09:35:29 +0300

    Continuations with data

---
+++ Chapter11/07 - Continuation On Success With Data/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew<DateTime>(() => DateTime.Now)
+            .ContinueWith(previous =>
+            {
+                Console.WriteLine($"The time from the previous task was {previous.Result}");
+            }).Wait();
+        }
+    }
+}
