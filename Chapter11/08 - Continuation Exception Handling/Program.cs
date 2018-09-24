78c03d7a043d5de39140c5cf0eef715a98f948e9
Conrad Akunga <conradakunga@gmail.com>
2018-09-24 10:03:09 +0300

    Exception handling with continuations

---
+++ Chapter11/08 - Continuation Exception Handling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var taskWork = Task.Factory.StartNew(() => throw new ApplicationException("Error!"));
+
+            var failure = taskWork.ContinueWith(prior =>
+            {
+                Console.WriteLine($"I failed: {prior.Exception.InnerException.GetType().Name}");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            var success = taskWork.ContinueWith(prior =>
+            {
+               Console.WriteLine($"I failed: {prior.Exception.InnerException.GetType().Name}");
+            }, TaskContinuationOptions.NotOnFaulted);
+            Console.ReadLine();
+        }
+    }
+}
