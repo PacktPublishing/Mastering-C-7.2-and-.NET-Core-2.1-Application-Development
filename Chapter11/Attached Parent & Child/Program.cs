0c05e430c2d23be0c0fb92cea6f68fbc1435585f
Conrad Akunga <conradakunga@gmail.com>
2018-12-09 17:45:41 +0300

    + Attached parent & child

---
+++ Chapter11/Attached Parent & Child/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Attached_Parent___Child
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parent = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Parent started ...");
+                var child = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Attached child started ...");
+                    Thread.SpinWait(TimeSpan.FromSeconds(5).Seconds);
+                    Console.WriteLine("Attached child completed ...");
+                }, TaskCreationOptions.AttachedToParent);
+            });
+            parent.Wait();
+            Console.WriteLine("Parent completed ...");
+        }
+    }
+}
+
