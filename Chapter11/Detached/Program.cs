bc9aefe5bdf9a5f2055760957351cc4207932d52
Conrad Akunga <conradakunga@gmail.com>
2018-12-09 18:01:54 +0300

    + Detached child

---
+++ Chapter11/Detached/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Detached
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parent = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Parent started ...");
+
+                var child = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Child started ... ");
+                    Thread.SpinWait(TimeSpan.FromSeconds(5).Seconds);
+                    Console.WriteLine("Child completed ...");
+                });
+            });
+
+            parent.Wait();
+            Console.WriteLine("Parent completed ...");
+        }
+    }
+}
