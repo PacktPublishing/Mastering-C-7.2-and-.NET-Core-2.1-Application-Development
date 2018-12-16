b669ee2e85fee87ace5dbcc81697139033b4d9e0
Conrad Akunga <conradakunga@gmail.com>
2018-12-16 19:49:53 +0300

    + Behavior suubject

---
+++ Chapter12/BehaviorSubject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace BehaviorSubject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new BehaviorSubject<string>("[Blank]");
+            //Register the subscription
+            PrintNames(subject);
+            //Add items
+            subject.OnNext("John");
+        }
+        static void PrintNames(IObservable<string> seq)
+        {
+            seq.Subscribe(val => Console.WriteLine($"{val} has been added"));
+        }
+    }
+}
