d8e9f195e7fcb28ae400cd22572cf60b37a9be97
Conrad Akunga <conradakunga@gmail.com>
2018-12-16 20:04:58 +0300

    + Async subject

---
+++ Chapter12/AsyncSubject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace AsyncSubject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new AsyncSubject<string>();
+            //Add items
+            subject.OnNext("John");
+            subject.OnNext("Mary");
+            subject.OnNext("Jane");
+            //Register the subscription
+            PrintNames(subject);
+            //You must complete to obtain the published value
+            subject.OnCompleted();
+        }
+        static void PrintNames(IObservable<string> seq)
+        {
+            seq.Subscribe(val => Console.WriteLine($"{val} has been added"));
+        }
+    }
+}
