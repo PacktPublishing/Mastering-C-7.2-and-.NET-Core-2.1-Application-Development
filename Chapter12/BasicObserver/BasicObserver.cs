94619c80660b16b034aef14c7631a4553deeee8a
Conrad Akunga <conradakunga@gmail.com>
2018-12-16 18:07:32 +0300

    + Simple observer

---
+++ Chapter12/BasicObserver/BasicObserver.cs
@@ -0,0 +1,20 @@
+using System;
+namespace BasicObserver
+{
+    public class SimpleObserver<T> : IObserver<T>
+    {
+        public void OnNext(T value)
+        {
+            Console.WriteLine("Sequence received {0}", value);
+        }
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Sequence threw exception {0}", error);
+        }
+        public void OnCompleted()
+        {
+            Console.WriteLine("Sequence terminated");
+        }
+    }
+}
+
---
+++ Chapter12/BasicObserver/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicObserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new SimpleObserver<string>();
+            o.OnNext("first");
+            o.OnNext("second");
+            o.OnNext("third");
+            o.OnCompleted();
+        }
+    }
+}
