b643d71509bf2ddcd2a8d716d21a1b756a1a6e20
Conrad Akunga <conradakunga@gmail.com>
2018-12-16 19:30:19 +0300

    + Replay with time window

---
+++ Chapter12/ReplayTimeWindow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace ReplayTimeWindow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Chache items for 3 seconds
+            var subject = new ReplaySubject<string>(TimeSpan.FromSeconds(3));
+            //Add items
+            subject.OnNext("John");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            subject.OnNext("Mary");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            subject.OnNext("Jane");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            //Register the subscription
+            PrintNames(subject);
+        }
+        static void PrintNames(IObservable<string> seq)
+        {
+            seq.Subscribe(val => Console.WriteLine($"{val} has been added"));
+        }
+    }
+}
