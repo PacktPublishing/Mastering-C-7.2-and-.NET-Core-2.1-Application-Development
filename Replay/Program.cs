d767504a78addb02742b168b9357a0bddfed6197
Conrad Akunga <conradakunga@gmail.com>
2018-12-16 19:20:23 +0300

    + Default relay

---
+++ Replay/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace Replay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new ReplaySubject<string>();
+            //Add items
+            subject.OnNext("John");
+            subject.OnNext("Mary");
+            subject.OnNext("Jane");
+            //Register the subscription
+            PrintNames(subject);
+        }
+        static void PrintNames(IObservable<string> seq)
+        {
+            seq.Subscribe(val => Console.WriteLine($"{val} has been added"));
+        }
+    }
+}
