667c6b84b3f595900d7d69a04b4ac720d493e05c
Conrad Akunga <conradakunga@gmail.com>
2018-12-16 19:07:30 +0300

    + Subject implementation

---
+++ Chapter12/NativeSubscription/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace NativeSubscription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new Subject<string>();
+            //Register the subscription
+            PrintNames(subject);
+            //Add items
+            subject.OnNext("John");
+            subject.OnNext("Mary");
+            subject.OnNext("Jane");
+        }
+        static void PrintNames(IObservable<string> seq)
+        {
+            seq.Subscribe(val => Console.WriteLine($"{val} has been added"));
+        }
+    }
+}
