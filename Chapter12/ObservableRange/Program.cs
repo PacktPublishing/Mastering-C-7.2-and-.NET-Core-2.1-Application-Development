cdd89fc29b144fea55edbaa0903d5318964c0520
Conrad Akunga <conradakunga@gmail.com>
2018-12-17 20:10:59 +0300

    + Observable range

---
+++ Chapter12/ObservableRange/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace ObservableRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var topFive = Observable.Range(1, 5);
+            topFive.Subscribe(val => Console.WriteLine(val),
+                () => Console.WriteLine("Completed"));
+        }
+    }
+}
