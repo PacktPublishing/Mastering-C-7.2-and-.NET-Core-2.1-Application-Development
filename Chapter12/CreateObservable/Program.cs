3b55b3383d542da4782f07594512a17007d59551
Conrad Akunga <conradakunga@gmail.com>
2018-12-17 19:53:34 +0300

    + Create observable

---
+++ Chapter12/CreateObservable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Timers;
+
+namespace CreateTestObservable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ob = Observable.Create<string>(
+            observer =>
+            {
+                var tim = new Timer();
+                tim.Interval = TimeSpan.FromSeconds(5).TotalMilliseconds;
+                // Publush clock ticks
+                tim.Elapsed += (sender, eArgs) => observer.OnNext("Recevied event");
+                // Attach the event halder
+                tim.Elapsed += OnTimerElapsed;
+                tim.Start();
+                return tim;
+            });
+
+            var subscription = ob.Subscribe(val => Console.WriteLine(val));
+            Console.Read();
+            subscription.Dispose();
+        }
+        private static void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"Clock tick: {e.SignalTime:hh:mm ss tt}");
+        }
+    }
+}
