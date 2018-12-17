6f0fda53a2865d07c35c159cd60bd01ae16b5963
Conrad Akunga <conradakunga@gmail.com>
2018-12-17 19:24:16 +0300

    + Observable blocks

---
+++ Chapter12/Observables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Observables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameReturn = Observable.Return<string>("Jane");
+
+            var emptyReturn = Observable.Empty<string>();
+
+            var neverReturn = Observable.Never<string>();
+
+            var throwReturn  = Observable.Throw<string>(new DivideByZeroException());
+        }
+    }
+}
