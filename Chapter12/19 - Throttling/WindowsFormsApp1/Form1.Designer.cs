067b6f87b0e8125d5c506fb02eea3cf018e6d6da
Conrad Akunga <conradakunga@gmail.com>
2018-12-18 18:26:49 +0300

    +Throttling support

---
+++ Chapter12/19 - Throttling/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX.Location = new System.Drawing.Point(84, 33);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(135, 31);
+            this.lblX.TabIndex = 0;
+            this.lblX.Text = "Waiting ...";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblY.Location = new System.Drawing.Point(84, 135);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(135, 31);
+            this.lblY.TabIndex = 1;
+            this.lblY.Text = "Waiting ...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 233);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+    }
+}
+
---
+++ Chapter12/19 - Throttling/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // define the events to capture
+            var moves = Observable.FromEventPattern<MouseEventHandler, MouseEventArgs>
+                (handler => MouseMove += handler, handler => MouseMove -= handler)
+                .Select(x => x.EventArgs.Location)
+                .Throttle(TimeSpan.FromSeconds(1));
+
+            // subscribe to the observable
+            var movesSubscription = moves.Subscribe(evt =>
+            {
+                lblX.Invoke((MethodInvoker)(() => lblX.Text = $"X is {evt.X}"));
+                lblY.Invoke((MethodInvoker)(() => lblY.Text = $"Y is {evt.Y}"));
+            });
+        }
+    }
+}
