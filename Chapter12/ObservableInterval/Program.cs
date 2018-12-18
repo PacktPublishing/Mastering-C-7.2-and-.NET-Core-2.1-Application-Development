42f40d13a4ac5580cd0b3c2a797dc660496d2ef4
Conrad Akunga <conradakunga@gmail.com>
2018-12-18 12:42:45 +0300

    + Observable interval

---
+++ Chapter12/ObservableInterval/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ObservableIntervalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Publish every second
+            var obs = Observable.Interval(TimeSpan.FromSeconds(1));
+
+            using (obs.Subscribe(x => Console.WriteLine($"Produced item #{x}")))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
