0e4930d04799821158bb75ee3daf2118da15ba32
Conrad Akunga <conradakunga@gmail.com>
2018-12-18 17:25:42 +0300

    + Change observable return type

---
+++ Chapter12/Observable Return Types/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // define the events to capture
+            var clicks = Observable.FromEventPattern<MouseEventHandler, MouseEventArgs>
+                (handler => MouseClick += handler, handler => MouseClick -= handler).Select(x => x.EventArgs.Location);
+
+            // subscribe to the observable
+            var clicksSubscription = clicks.Subscribe(evt =>
+            {
+                lblX.Text = $"X is {evt.X}";
+                lblY.Text = $"Y is {evt.Y}";
+            });
+        }
+    }
+}
