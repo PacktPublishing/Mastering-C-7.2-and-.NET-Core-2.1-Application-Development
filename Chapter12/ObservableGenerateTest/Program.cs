61825c07cd8f8c260a30e11aeb5b8b1dfc442044
Conrad Akunga <conradakunga@gmail.com>
2018-12-18 12:28:17 +0300

    + Add files

---
+++ Chapter12/ObservableGenerateTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace ObservableGenerateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obs = Observable.Generate('A',               // This is the initial state
+                                    x => (char)(x) <= 'Z',   // This is the exit condition
+                                    x => (char)(x + 1),      // This is the increment step
+                                    x => (char)x,            // This is what to output
+                                    Scheduler.Default);
+
+            using (obs.Subscribe(x => Console.WriteLine($"Produced {x}")))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
