28d1ae5f3a34d799b12a4a7260fb685ca2fed8b6
Conrad Akunga <conradakunga@gmail.com>
2018-12-18 13:08:10 +0300

    + Obervable Timer Overload

---
+++ Chapter12/ObservableTimerTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ObservableTimerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start after 5 seconds and publish every five seconds
+            var obs = Observable.Timer(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5));
+
+            Console.WriteLine($"{DateTime.Now:hh:mm ss}");
+            
+            using (obs.Subscribe(x => Console.WriteLine($"Produced item #{x} : {DateTime.Now:hh:mm ss}")))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
