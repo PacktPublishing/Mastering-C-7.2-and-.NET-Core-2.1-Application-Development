bf7f2853fea2d0bc46642cacad8dd4115800f510
Conrad Akunga <conradakunga@gmail.com>
2018-12-18 13:23:45 +0300

    + Observable timer

---
+++ Chapter12/ObservableTimerSingle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ObservableTimerSingle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start after 5 seconds and publish once
+            var obs = Observable.Timer(TimeSpan.FromSeconds(5));
+            Console.WriteLine($"{DateTime.Now:hh:mm ss}");
+            using (obs.Subscribe(x => Console.WriteLine($"Produced item #{x} : {DateTime.Now:hh:mm ss}")))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
