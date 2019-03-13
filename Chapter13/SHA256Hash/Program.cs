bc6a270b88a282a31c18bbe198d22cc7eb11db45
Conrad Akunga <conradakunga@gmail.com>
2019-03-13 18:57:45 +0300

    # SHA265

---
+++ Chapter13/SHA256Hash/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Serilog;
+
+namespace SHA256Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            using (var sha = SHA256.Create())
+            {
+                Log.Information("The hash for {input1} is {hash}", "12345", Hash(sha, "12345"));
+                Log.Information("The hash for {input2} is {hash}", "12346", Hash(sha, "12346"));
+            }
+        }
+        static string Hash(SHA256 sha, string input)
+        {
+            return string.Join("", sha.ComputeHash(Encoding.Default.GetBytes(input)).SelectMany(x => x.ToString("x2")));
+        }
+    }
+}
