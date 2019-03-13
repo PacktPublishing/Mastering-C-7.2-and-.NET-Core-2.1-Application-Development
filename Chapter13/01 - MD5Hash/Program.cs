b4a67b14028219705e8214ec7acaacf476a99719
Conrad Akunga <conradakunga@gmail.com>
2019-03-13 18:36:55 +0300

    # Fix logging

--- Chapter13/01 - MD5Hash/Program.cs
+++ Chapter13/01 - MD5Hash/Program.cs
@@ -12,9 +12,9 @@ namespace MD5Hash
             Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
             using (var md5 = MD5.Create())
             {
-                Log.Information($"The hash for {12345} is {Hash(md5, "12345")}");
-                Log.Information($"The hash for {12345} is {Hash(md5, "12345")}");
-                Log.Information($"The hash for {12346} is {Hash(md5, "12346")}");
+                Log.Information("The hash for {input} is {hash}", "12345", Hash(md5, "12345"));
+                Log.Information("The hash for {input} is {hash}", "12345", Hash(md5, "12345"));
+                Log.Information("The hash for {input} is {hash}", "12346", Hash(md5, "12346"));
             }
         }
         static string Hash(MD5 md5, string input)
