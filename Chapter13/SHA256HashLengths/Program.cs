5af14517b10379ecce2cdd95e4b2d8694bbfd630
Conrad Akunga <conradakunga@gmail.com>
2019-03-14 19:04:05 +0300

    + Hash lengths

---
+++ Chapter13/SHA256HashLengths/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Serilog;
+
+namespace SHA256HashLengths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            using (var sha = SHA256.Create())
+            {
+                Log.Information("The hash for {input1} is {hash}", "A", Hash(sha, "A"));
+                Log.Information("The hash for {input2} is {hash}", "The Quick Brown Fox Jumped",
+                    Hash(sha, "The Quick Brown Fox Jumped"));
+            }
+        }
+        static string Hash(SHA256 sha, string input)
+        {
+            var hash = string.Join("", sha.ComputeHash(Encoding.Default.GetBytes(input)).Select(x => x.ToString("x2")));
+            return $"{hash} ({hash.Length})";
+        }
+    }
+}
