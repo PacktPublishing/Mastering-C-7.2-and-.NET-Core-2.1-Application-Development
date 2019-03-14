55a52a77861d599be194271a8c5f07ceabec8f5b
Conrad Akunga <conradakunga@gmail.com>
2019-03-14 19:28:46 +0300

    + File hashing

---
+++ Chapter13/FileHashing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Serilog;
+
+namespace FileHashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            using (var sha = SHA256.Create())
+            {
+                Log.Information("The hash for the file is is {hash}", Hash(sha, File.OpenRead("test.txt")));
+            }
+        }
+        static string Hash(SHA256 sha, Stream file)
+        {
+            return string.Join("", sha.ComputeHash(file).Select(x => x.ToString("x2")));
+        }
+    }
+}
