d8b8abefc61c2f24d3f6885dabd0ff0913f16747
Conrad Akunga <conradakunga@gmail.com>
2019-03-15 22:41:02 +0300

    + RSA Encryption

--- Chapter13/11 - RSAEncryption/Program.cs
+++ Chapter13/11 - RSAEncryption/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Serilog;
 
 namespace RSAEncryption
 {
@@ -6,7 +10,24 @@ namespace RSAEncryption
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                // Get the file path to store the keys
+                var keysPath = Path.Combine(Path.GetTempPath(), "keys.xml");
+                // Get the file path to store encrypted data
+                var encryptedDataPath = Path.Combine(Path.GetTempPath(), "Encrypted.bin");
+                // Save the public and private keys to disk
+                File.WriteAllText(keysPath, rsa.ToXmlString(true));
+                // Now encrypt our custom text
+                byte[] encryptedData = rsa.Encrypt(Encoding.Default.GetBytes("This is a test to encrypt"), true);
+                // Write this text to screen
+                Log.Information("The encrypted text is {text}", Encoding.Default.GetString(encryptedData));
+                // Save the encrypted data to file
+                File.WriteAllBytes(encryptedDataPath, encryptedData);
+                Log.Information("Keys were saved to {path}", keysPath);
+                Log.Information("Encrypted data was saved to {path}", encryptedDataPath);
+            }
         }
     }
 }
