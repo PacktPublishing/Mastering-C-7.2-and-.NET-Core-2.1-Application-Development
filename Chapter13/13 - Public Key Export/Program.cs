6f40da14790f8f2a68bfa1938da369b88161a062
Conrad Akunga <conradakunga@gmail.com>
2019-03-17 19:13:15 +0300

    # Cleanup + Public  & private key export

---
+++ Chapter13/13 - Public Key Export/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using Serilog;
+
+namespace PublicKeyExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                // Get the file path to store the keys
+                var publicKeyPath = Path.Combine(Path.GetTempPath(), "PublicKey.xml");
+                var publicAndPrivateKeyPath = Path.Combine(Path.GetTempPath(), "AllKeys.xml");
+                // Save the keys to disk
+                File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+                File.WriteAllText(publicAndPrivateKeyPath, rsa.ToXmlString(true));
+                Log.Information("Public Key was saved to {path}", publicKeyPath);
+                Log.Information("Public & Private Keys were saved to {path}", publicAndPrivateKeyPath);
+            }
+        }
+    }
+}
